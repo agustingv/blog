34c93450ef5b2aa8da848d794ba6fe7e90783042
Agustín García <contacto@agustin-garcia.es>
2023-02-06 04:14:56 +0100

    update api-layer

--- core/scripts/cron-curl.sh
+++ core/scripts/cron-curl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl --silent --compressed http://gelr.es/cron/8fZr0J-oMauO6Vnl4rDnuhYqFhEtQKq6ig2EN-oBwz1Ye8oCoD49u1uV4Rj5xi2Ib6jfOJQ4XQ
+curl --silent --compressed https://gelr.es/cron/8fZr0J-oMauO6Vnl4rDnuhYqFhEtQKq6ig2EN-oBwz1Ye8oCoD49u1uV4Rj5xi2Ib6jfOJQ4XQ
